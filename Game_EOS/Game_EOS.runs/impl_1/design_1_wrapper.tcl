8de26ffb8463b68c12becca8c8323cddaa4de644
Cyril Knops <cyrilknops@gmail.com>
2021-01-10 19:57:34 +0100

    new XSA file

--- Game_EOS/Game_EOS.runs/impl_1/design_1_wrapper.tcl
+++ Game_EOS/Game_EOS.runs/impl_1/design_1_wrapper.tcl
@@ -17,7 +17,7 @@ proc create_report { reportName command } {
   }
 }
 namespace eval ::optrace {
-  variable script "E:/school/EOS/Opdracht/EOS/Game_EOS/Game_EOS.runs/impl_1/design_1_wrapper.tcl"
+  variable script "C:/Users/cyril/EOS/Eindoefening/EOS/Game_EOS/Game_EOS.runs/impl_1/design_1_wrapper.tcl"
   variable category "vivado_impl"
 }
 
@@ -122,9 +122,9 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param chipscope.maxJobs 3
+  set_param chipscope.maxJobs 2
   open_checkpoint design_1_wrapper_routed.dcp
-  set_property webtalk.parent_dir E:/school/EOS/Opdracht/EOS/Game_EOS/Game_EOS.cache/wt [current_project]
+  set_property webtalk.parent_dir C:/Users/cyril/EOS/Eindoefening/EOS/Game_EOS/Game_EOS.cache/wt [current_project]
 set_property TOP design_1_wrapper [current_fileset]
 OPTRACE "read constraints: write_bitstream" START { }
 OPTRACE "read constraints: write_bitstream" END { }
--- Game_EOS/Game_EOS.runs/synth_1/runme.sh
+++ Game_EOS/Game_EOS.runs/synth_1/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='E:/school/EOS/Opdracht/EOS/Game_EOS/Game_EOS.runs/synth_1'
+HD_PWD='C:/Users/cyril/EOS/Eindoefening/EOS/Game_EOS/Game_EOS.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
